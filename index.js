b988c55e0b06a1007b475c6d59a62fe4dbdad911
Bryant Torres <brt9029@gmail.com>
2018-05-23 16:44:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for (var i = 0; i < musicians.length ; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  var num = 0
+  while (num < facts.length){
+    facts[num] = `${facts[num]}` + "!!!"
+    num ++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number){
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+  } while(number < 15)
+}
